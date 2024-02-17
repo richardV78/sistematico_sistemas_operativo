c08dec951653e79e5e24620dc238508b99d208a1
Richard <richardvalles94@gmail.com>
2024-02-17 09:13:03 -0600

    modificaciones del codigo

--- clase5.py
+++ clase5.py
@@ -1,13 +1,4 @@
-#ejemplo de recursividad 
-def factorial (n):
-    if n == 0:
-        return 1
-    else: 
-        return n * factorial(n-1)
-
-# ejemplo de uso
-numero = 5
-resultado = factorial(numero)
-print (f"El factorial de {numero} es : {resultado}")
+#lsitas
+lista = [  1, 2, 3]
 
 
